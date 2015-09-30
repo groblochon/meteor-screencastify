5bb95e523486620783eca984746678c7e66249b0
groblochon <groblochon@gmail.com>
2015-09-30 11:37:20 +0200

    Add tracer

--- package.js
+++ package.js
@@ -1,11 +1,21 @@
 Package.describe({
-  name: 'groblochon:screencastify',
-  version: '0.0.1',
+  name: 'picman:screencastify',
+  version: '0.0.2',
   summary: "Screencastify for Meteor",
   git: 'https://github.com/groblochon/meteor-screencastify',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
+  api.add_files(".npm/plugin/compileTraceur/node_modules/traceur/bin/traceur-runtime.js");
   api.add_files('castify-api.js', 'client');
+});
+
+Package._transitional_registerBuildPlugin({
+  name: "compileTraceur",
+  use: [],
+  sources: [
+    "plugin/compile-traceur.js"
+  ],
+  npmDependencies: {"traceur": "0.0.41"}
 });
